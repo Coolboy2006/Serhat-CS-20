4b549afa045361ac52374d945d6b2832662f53b9
16189009 <16189009@L815WD220011.edu.cbe.ab.ca>
2022-09-12 13:56:23 -0600

    1:55PM

--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -1,8 +1,26 @@
 
-public class TicTacToe {
+public class TicTacToe 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                --------------------");
+		System.out.println("                          |/n");
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                --------------------");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("                --------------------");
+	
+	
 
 	}
 
