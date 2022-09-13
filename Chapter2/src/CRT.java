797f1178d1868a834725cbac8690b30521e54b2c
16189009 <16189009@L815WD220011.edu.cbe.ab.ca>
2022-09-13 15:27:33 -0600

    3:27PM

---
+++ Chapter2/src/CRT.java
@@ -0,0 +1,23 @@
+
+public class CRT {
+
+	public static void main(String[] args) 
+	{
+	
+		/*
+		 3.
+		    TestAverages.java
+		    Calculates test averages
+		    Serhat
+		    September 13th, 2022
+		    
+		 4. //The AverageScore displays the average set of scores
+		 
+		 
+		 7. //The difference between a Bytecode and a source code is a Bytecode is compiled by java while the source code is compiled by the programmer 
+		 
+		 12. When you put system.out.print() it will display the output in one line while system.out.println() will display the output in different lines 
+		 */
+	}
+
+}
--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -4,24 +4,18 @@ public class TicTacToe
 
 	public static void main(String[] args) 
 	{
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("                ---------------------");
-		System.out.println("                          |");
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("                --------------------");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("                --------------------");
-	
-	
-
+		System.out.println("                       |         |");
+		System.out.println("                       |         |");
+		System.out.println("                       |         |");
+		System.out.println("                -----------------------");
+		System.out.println("                       |         |");
+		System.out.println("                       |    X    |");
+		System.out.println("                       |         |");
+		System.out.println("                -----------------------");
+		System.out.println("                       |         |");
+		System.out.println("                       |         |");
+		System.out.println("                       |         |");
 	}
-
 }
+		
+		
